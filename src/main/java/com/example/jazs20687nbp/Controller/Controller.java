536a304531f025064678b4bbd1e197c2ba995b38
s20687-pj <s20687-pj@pjwstk.edu.pl>
2021-07-04 21:02:50 +0200

    adding working method for avarage

--- src/main/java/com/example/jazs20687nbp/Controller/Controller.java
+++ src/main/java/com/example/jazs20687nbp/Controller/Controller.java
@@ -4,6 +4,7 @@ import com.example.jazs20687nbp.Model.Root;
 import com.example.jazs20687nbp.Service.GoldService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,8 @@ public class Controller {
     public Controller(GoldService goldService) {
         this.goldService = goldService;
     }
-    
+
+    @ApiOperation(value = "pobierz średnia cene złota z zakresu dni", notes = "podaj date poczatkowa i koncowa")
     @GetMapping("/nbp2/{from}/{to}")
     public ResponseEntity <Double> api2 (@ApiParam(value = "Data początek",
             example = "2012-05-05") @PathVariable String from, @ApiParam(value = "Data koniec",
