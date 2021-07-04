ef4ca09fe88f52b35390b8758b260a70f8b66826
s20687-pj <s20687-pj@pjwstk.edu.pl>
2021-07-04 19:05:46 +0200

    initial commit

---
+++ src/main/java/com/example/jazs20687nbp/JazS20687NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs20687nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS20687NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS20687NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jazs20687nbp/JazS20687NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs20687nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS20687NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
